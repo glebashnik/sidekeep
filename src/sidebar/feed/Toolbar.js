a336f4d7af819ac0d27771badc9ac6b6d2ec9e8b
Gleb Sizov <Gleb Sizov>
2015-11-17 11:54:58 +0100

    Added shadows to toolbar and FloatingMenuButton.

--- src/sidebar/feed/Toolbar.js
+++ src/sidebar/feed/Toolbar.js
@@ -32,7 +32,8 @@ export default class ToolBar extends React.Component {
                 display: 'flex',
                 flexDirection: 'column',
                 width: '100%',
-                zIndex: 5
+                zIndex: 5,
+                boxShadow: '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)'
             },
             tools: {
                 display: 'flex',
